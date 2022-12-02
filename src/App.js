6387ceb5a4f63c270892bf5022189d58843855db
arkk200 <arkk20000@gmail.com>
2022-12-03 02:40:22 +0900

    ADD :: Manage 폴더에 Front-end 레포지터리 병함

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
+  Routes,
 } from 'react-router-dom';
-
+import Info from './Manage/components/mainpage/Info';
+import UseElec from './Manage/components/useElec/useElec';
 import WrapContainer from "./WrapContainer";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/">
-          <WrapContainer />
-        </Route>
-        <Route path="/three">
-          <div>test</div>
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<WrapContainer />} />
+        <Route path="info" element={<Info />} />
+        <Route path="useElec" element={<UseElec />} />
+      </Routes>
     </Router>
   );
 }
