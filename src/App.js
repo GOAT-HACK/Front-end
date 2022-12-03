9514eabca9ef677bad9e74c77a11e827f94e1343
arkk200 <arkk20000@gmail.com>
2022-12-03 09:00:30 +0900

    ADD :: accDevelop 페이지 제작

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
+import AccDevelop from './Manage/components/accdevelop/accDevelop';
 import Info from './Manage/components/mainpage/Info';
 import TotalManage from './Manage/components/totalmanage/totalManage';
 import UseElec from './Manage/components/useElec/useElec';
@@ -13,9 +14,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<WrapContainer />} />
-        <Route path="info" element={<Info />} />
-        <Route path="useElec" element={<UseElec />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/useElec" element={<UseElec />} />
         <Route path="/totalManage" element={<TotalManage />} />
+        <Route path="/accdevelop" element={<AccDevelop />} />
       </Routes>
     </Router>
   );
